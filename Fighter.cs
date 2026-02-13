bb7bc8f8c3ff2644dde34e3e86427adfdf261137
imthatguy123yt <33688@tiftschools.com>
2026-02-13 16:43:57 -0500

    Create Fighter.cs

---
+++ Fighter.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 7f;
+    public int health = 100;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+}
